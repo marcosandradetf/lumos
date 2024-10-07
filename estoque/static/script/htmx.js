1ff2f59645085c7835ef0ebb7707bee68a711aaf
Marcos Tulio <marcostfandrade@proton.me>
2024-10-06 23:39:49 -0300

    feat: htmx response 2

--- estoque/static/script/htmx.js
+++ estoque/static/script/htmx.js
@@ -1,4 +1,4 @@
-    document.body.addEventListener('htmx:configRequest', function(event) {
-        const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-        event.detail.headers['X-CSRFToken'] = csrfToken;
-    });
+function updateMessages(event) {
+        // Atualiza o conteúdo das mensagens diretamente a partir da resposta
+        document.getElementById('messages-container').innerHTML = event.detail.xhr.responseText.split('<div id="messages-container">')[1];
+    }
--- estoque/views.py
+++ estoque/views.py
@@ -10,24 +10,6 @@ from django.shortcuts import render
 from .models import Materiais
 
 def view_material(request):
-    # # Se for uma requisição POST, processa o formulário
-    # if request.method == 'POST':
-    #     materiais_form = MateriaisForm(request.POST)
-    #     if materiais_form.is_valid():
-    #         materiais_form.save()  # Salva o novo material no banco de dados
-    #         messages.success(request, 'Material cadastrado com sucesso!')  # Mensagem de sucesso
-    #
-    #         return redirect('cadastrar_materiais')  # Redireciona para a mesma página após salvar
-    # else:
-    #     materiais_form = MateriaisForm()
-    #
-    # materiais_obj = Materiais.objects.all()
-    #
-    # context = {
-    #     'form': materiais_form,  # O formulário para cadastro dados dos materiais
-    #     'messages': messages.get_messages(request),
-    #     'materiais_obj': materiais_obj,  # Inclui os campos no contexto
-    # }
     materiais_obj = Materiais.objects.all()
     materiais_form = MateriaisForm()
     context = {'form': materiais_form,
@@ -38,17 +20,17 @@ def view_material(request):
 def insert_material(request):
     materiais_form = MateriaisForm(request.POST)
     materiais_obj = Materiais.objects.all()
-    context = {'materiais_form': materiais_form,
+    context = {'form': MateriaisForm(),
                'materiais_obj': materiais_obj,
                'messages': messages.get_messages(request),}
     if materiais_form.is_valid():
         materiais_form.save()  # Salva o novo material no banco de dados
         messages.success(request, 'Material cadastrado com sucesso!')  # Mensagem de sucesso
-        response = render_to_string('materiais/tabelas/tabela_materiais.html', context)
+        response = render_to_string('materiais/gerenciar/client.html', context)
         return HttpResponse(response)
 
     messages.error(request, "Erro ao cadastrar materiais")
-    response = render_to_string('materiais/tabelas/tabela_materiais.html', context)
+    response = render_to_string('materiais/gerenciar/client.html', context)
     return HttpResponse(response)
 
 
